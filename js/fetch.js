13fa065fe3fe743984ef4301563c95cd50452cd3
Настя <smelw@list.ru>
2022-12-04 21:29:39 +0300

    async await

--- js/fetch.js
+++ js/fetch.js
@@ -1,31 +1,44 @@
 const request = "https://jsonplaceholder.typicode.com/comments";
 
-function sendRequest(method, url, body = null) {
-  const headers = {
-    "Content-Type": "application/json",
-  };
+// function sendRequest(method, url, body = null) {
+//   const headers = {
+//     "Content-Type": "application/json",
+//   };
 
-  return fetch(url, { method: method, body: JSON.stringify(body), headers: headers }).then((response) => {
-    if (response.ok) {
-        return response.json();
-    }
-    return response.json().then(error => {
-        const e = new Error('Error!')
-        e.data = error
-        throw e
-    })
-  });
-}
+//   return fetch(url, { method: method, body: JSON.stringify(body), headers: headers }).then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     return response.json().then((error) => {
+//       const e = new Error("Error!");
+//       e.data = error;
+//       throw e;
+//     });
+//   });
+// }
 
 // sendRequest("GET", request)
 //   .then((data) => console.log(data))
 //   .catch((err) => console.log(err));
 
-const body = {
-  name: "Vals",
-  age: 25,
-};
+// const body = {
+//   name: "Vals",
+//   age: 25,
+// };
 
-sendRequest("POST", request, body)
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// sendRequest("POST", request, body)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+async function fetchAsyncTodos() {
+  try {
+    const response = await fetch(request);
+    const data = await response.json();
+    console.log("Data", data);
+  } catch (e) {
+    console.error(e);
+  } finally {
+  }
+}
+
+fetchAsyncTodos();
